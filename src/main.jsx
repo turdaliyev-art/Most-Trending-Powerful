039d71cb60b5f6d3df360652e653a09e1b9ea554
turdaliyev-art <bturdaliyev49@gmail.com>
2026-04-23 22:26:13 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/Most-Trending-Powerful">
+
   <StrictMode>
     <App />
   </StrictMode>,
+  </BrowserRouter>
 )
